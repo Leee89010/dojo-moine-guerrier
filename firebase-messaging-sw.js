ad6150b2334813cdfed767949120e2e5b55d4897
Leee89010 <leolambert21@gmail.com>
2026-01-25 15:28:52 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,8 +14,6 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-    console.log("📩 Notification en arrière-plan :", payload);
-
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
         icon: "icon-192.png"
