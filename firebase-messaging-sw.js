2d8c4f79e165f9c2c15f298fe03592604c601cf2
Leee89010 <leolambert21@gmail.com>
2026-01-25 15:23:26 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBl5qUBgdizyFhZsDDQOXM_JlFHtn7PZj0",
+  authDomain: "dojo-du-moine-guerrier.firebaseapp.com",
+  projectId: "dojo-du-moine-guerrier",
+  storageBucket: "dojo-du-moine-guerrier.firebasestorage.app",
+  messagingSenderId: "395565857197",
+  appId: "1:395565857197:web:dedd212901fcd4835e8924",
+  measurementId: "G-WYNDY2KDP5"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    console.log("📩 Notification en arrière-plan :", payload);
+
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: "icon-192.png"
+    });
+});
